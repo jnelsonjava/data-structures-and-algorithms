00b5c597ff006afae87276496847848f5cd8d4f7
Jack Nelson <jnelson.java@gmail.com>
2020-11-24 16:30:08 -0800

    scaffolded

---
+++ hrank/src/main/java/hrank/basics/RoundingGrades.java
@@ -0,0 +1,9 @@
+package hrank.basics;
+
+import java.util.List;
+
+public class RoundingGrades {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        
+    }
+}
---
+++ hrank/src/test/java/hrank/basics/RoundingGradesTest.java
@@ -0,0 +1,26 @@
+package hrank.basics;
+
+import org.junit.Test;
+
+public class RoundingGradesTest {
+    @Test
+    public void testRoundUp() {
+
+    }
+
+    @Test
+    public void testRoundDown() {
+    }
+
+    @Test
+    public void testFailing() {
+    }
+
+    @Test
+    public void testEmptyList() {
+    }
+
+    @Test
+    public void testMixedResults() {
+    }
+}
